1739afe77e3cf0acc7e49f05b23f89e4bfcbcfe6
Graham Pye <frahamp@gmail.com>
2020-10-18 15:00:53 +0100

    Initial setup and deployment script

---
+++ deployment/deployToAws.ps1
@@ -0,0 +1,35 @@
+param(
+    [string]
+    $s3BucketName,
+
+    [string]
+    $version
+)
+
+#todo: check bucket exists and accessible
+
+$codeZipFileName = "code.zip"
+$dependenciesZipFileName = "dependencies.zip"
+
+Write-Host "Zipping code and dependencies"
+
+7z a $codeZipFileName .\src\scripts\* | Out-Null
+7z a $dependenciesZipFileName .\src\dependencies\* | Out-Null
+
+Write-Host "Finished zipping code and dependencies"
+
+Write-Host "Uploading files to s3"
+
+aws s3 cp $codeZipFileName "s3://$($s3BucketName)/v$($version)/$($codeZipFileName)"
+aws s3 cp $dependenciesZipFileName "s3://$($s3BucketName)/v$($version)/$($dependenciesZipFileName)"
+
+Write-Host "Finished uploading files to s3"
+
+Write-Host "Remove zip files"
+
+Remove-Item $codeZipFileName
+Remove-Item $dependenciesZipFileName
+
+Write-Host "Finished remove zip files"
+
+terraform apply
---
+++ src/scripts/logGroupChecker.js
@@ -0,0 +1,7 @@
+const AWSXRay = require('aws-xray-sdk');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+
+
+exports.handler = (event, context) => {
+
+};
