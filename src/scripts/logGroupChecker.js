b73fa977ca2e2906214256210f4e194504ae2111
Graham Pye <frahamp@gmail.com>
2020-10-18 16:13:37 +0100

    testing getting groups

--- src/scripts/logGroupChecker.js
+++ src/scripts/logGroupChecker.js
@@ -1,7 +1,39 @@
 const AWSXRay = require('aws-xray-sdk');
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
+async function getLogGroups(nextToken) {
+    var client = new AWS.CloudWatchLogs({
+        region: process.env.AWS_REGION
+    });
 
-exports.handler = (event, context) => {
+    return new Promise((resolve, reject) => {
+        var params = {
+            limit: '2'
+        };
 
+        if (nextToken){
+            params.nextToken = nextToken;
+        }
+        client.describeLogGroups(params, function (err, data) {
+            if (err) { 
+                console.log(err, err.stack); 
+                reject(err);
+            }
+            else { 
+                console.log(data);
+                resolve(data);
+            }
+        });
+    });
+}
+
+
+exports.handler = async (event, context) => {
+    let nextToken = "";
+    await getLogGroups(nextToken).then(
+        
+    ).catch(err => {
+        console.error(err);
+        throw err;
+    });
 };
